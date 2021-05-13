82e89bca24d6b19d5c94fff8866a05ef41dfb1dc
Seokbin Yoon <nyoongoon@naver.com>
2021-05-13 21:14:22 +0900

    [프로그래머스 챌린지 다시보기_Sol2]

---
+++ Programmers/src/ProgrammersChallenge/May13th_Sol2.java
@@ -0,0 +1,12 @@
+package ProgrammersChallenge;
+
+public class May13th_Sol2 {
+	//5.13 프로그래머스 다시보기
+    //자바 int는 4바이트 이므로 16비트. (16자리 숫자) -> 이진수 변환후 0채우기.
+    public static void main(String[] args){
+        int a = 2;
+        String str = Integer.toBinaryString(a);
+        System.out.println(String.format("%016d", Integer.parseInt(str)));
+
+    }
+}
