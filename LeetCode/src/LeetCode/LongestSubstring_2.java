ddc3cd34f4c02b98b6a43793b1f718704a8b40cf
Seokbin Yoon <nyoongoon@naver.com>
2021-05-22 14:01:51 +0900

    [LeetCode]LongestSubstring_2(Map)

--- LeetCode/src/LeetCode/LongestSubstring_2.java
+++ LeetCode/src/LeetCode/LongestSubstring_2.java
@@ -1,21 +1,29 @@
 package LeetCode;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class LongestSubstring_2 {
 	public int lengthOfLongestSubstring(String s) {
-	  if (s.length()==0) return 0;
-      HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-      int max=0;
-      for (int i=0, j=0; i<s.length(); ++i){
-          if (map.containsKey(s.charAt(i))){
-              j = Math.max(j,map.get(s.charAt(i))+1);
-          }
-          map.put(s.charAt(i),i);
-          max = Math.max(max,i-j+1);
-      }
-      return max;
+		Map<Character, Integer> map = new HashMap<>();
+		int max = 0;
+		for (int i = 0, j = 0; i < s.length(); i++) {
+			if (map.containsKey(s.charAt(i))) {
+				// 중복 발생! -> 중복되었던 요소의 마지막 인덱스 + 1
+				// 새로운 서브스트링의 첫번째 인덱스
+				// max 계산을 통해서 중복된 값 끼리 비교하여 늦게 나온 값을 뽑기.
+				// 그래야 새로운 서브스트링 max비교가 가능.
+				j = Math.max(j, map.get(s.charAt(i)) + 1);
+
+			}
+			map.put(s.charAt(i), i);
+			// i는 현재 인덱스. j는 서브스트링의 첫번째 인덱스
+			// max =기존 서브스트링의 길이, i-j+1은 새로운 서브스트링의 길이
+			max = Math.max(max, i - j + 1);
+		}
+		return max;
 	}
+}
 	/*public int lengthOfLongestSubstring(String s) {
         int max = 0;
         int temp = 0;
@@ -39,4 +47,4 @@ public class LongestSubstring_2 {
 		}
 		return max;
     }*/
-}
+
