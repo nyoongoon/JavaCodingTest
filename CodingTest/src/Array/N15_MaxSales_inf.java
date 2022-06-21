033e45a725955ead800accc23c371faedbd4a67d
nyoongoon93 <68639744+nyoongoon@users.noreply.github.com>
2022-06-21 12:53:32 +0900

    윈도우 슬라이드

--- CodingTest/src/Array/N15_MaxSales_inf.java
+++ CodingTest/src/Array/N15_MaxSales_inf.java
@@ -1,25 +1,34 @@
 package Array;
 
+import java.util.Scanner;
+
 public class N15_MaxSales_inf {
-    Scanner stdIn = new Scanner(System.in);
-    int n = stdIn.nextInt();
-    int[] sales = new int [n];
-    int k = stdIn.nextInt();
-    int max = Integer.MIN_VALUE;
+    public static void main(String[] args){
+        Scanner stdIn = new Scanner(System.in);
+        int n = stdIn.nextInt();
+        int[] sales = new int [n];
+        int k = stdIn.nextInt();
+        int max = Integer.MIN_VALUE;
 
-    //k까지 미리 합하기
-    int sum = 0;
-		for(int i = 0; i<k; i++){
-        sum += sales[i];
+        for(int i = 0; i<sales.length; i++){
+            sales[i] = stdIn.nextInt();
+        }
+
+        //k까지 미리 합하기
+        int sum = 0;
+        for(int i = 0; i<k; i++){
+            sum += sales[i];
+        }
+        max = Math.max(max, sum);
+        //System.out.println("0~2" + sum);
+
+        for(int i = k; i<sales.length; i++){
+            sum += sales[i];
+            sum -= sales[i-k];
+            // if(Math.max(tmp, max)) max = tmp; return이 boolean이 아니라, 큰 값 리턴 임.
+            max = Math.max(max, sum);
+        }
+
+        System.out.println (max);
     }
-
-		System.out.println("0~2" + sum);
-
-		for(int i = k; i<sales.length; i++){
-        int tmp = sum + sales[i] - sales[i-k];
-        // if(Math.max(tmp, max)) max = tmp; return이 boolean이 아니라, 큰 값 리턴 임.
-        max = Math.max(max, tmp);
-    }
-
-		System.out.println (max);
 }
---
+++ CodingTest/src/Array/N16_MaxConsecutiveNumberSubSequence.java
@@ -0,0 +1,39 @@
+package Array;
+
+import java.util.Scanner;
+
+public class N16_MaxConsecutiveNumberSubSequence {
+    //최대 연속 부분 수
+    public static void main(String[] args){
+        //n개로 이루어진 수열
+        //연속 부분 수열 합이 특정 숫자 M 몇번 있는가.
+        // 윈도우 슬라이드 => 슬라이드 크기 키우면서 움직이면 될 듯
+        Scanner stdIn = new Scanner(System.in);
+        int n = stdIn.nextInt();
+        int[] arr = new int[n];
+        int m = stdIn.nextInt();
+        int subArrCtn = 0;
+        int surArrLen = 1;
+
+        for(int i = 0; i<arr.length; i++){
+            arr[i] = stdIn.nextInt();
+        }
+
+        for(int i = 1; i <= arr.length; i++){ // 윈도우 슬라이드 길이 1부터 n까지.
+            int curSum = 0;
+            for(int j = 0; j<i; j++){ // 초기 sum 구하기
+                curSum += arr[j];
+            }
+            if(curSum == m) subArrCtn++;
+
+            for(int j = 0; j<arr.length-i; j++){
+                curSum -= arr[j];
+                curSum += arr[j+i]; // 인덱스 범위 초과 주의 !
+
+                if(curSum == m) subArrCtn++;
+            }
+        }
+
+        System.out.println(subArrCtn); //런타임 에러 !!!
+    }
+}
