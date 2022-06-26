d9dfe1f5a903093f7172a0a1fb4cb99401a5628f
nyoongoon93 <68639744+nyoongoon@users.noreply.github.com>
2022-06-26 14:10:54 +0900

    2022 카카오블라인드 lv3 사라지는발판(틀림)

--- CodingTest/src/DFS/MissingScaffolding_2022KakaoBlind.java
+++ CodingTest/src/DFS/MissingScaffolding_2022KakaoBlind.java
@@ -2,155 +2,118 @@ package DFS;
 import java.util.*;
 public class MissingScaffolding_2022KakaoBlind { /// 실패함.... 테스트 케이스에서 이상한 움직임이 나옴 !!!
 
-        //static int turns = 1;
-        static int max = Integer.MIN_VALUE;
-        public int solution(int[][] board, int[] aloc, int[] bloc) {
-            int deps = 0;
-            // 처음캐릭터 위치 발판 있는 곳.
-            // 발반으로만 이동 --> bfs..?
-            // 발판은 밝고 있던 캐릭터가 다른 발판을 밝으면 사라짐 !
-            // 상하좌우 인접한 4칸
-            // 갈 곳이 없으면 패배
-            // 같은 발판위에서 상대 플레이어 캐릭터가 이동, 내 발판 사라지면 패배.
-            // A가 시작
 
-            // 1. 트리 그리기 -> 움직임 함수 만들기
+    static int max = Integer.MAX_VALUE;
+    public int solution(int[][] board, int[] aloc, int[] bloc) {
 
-            // 2. 재귀 타고 올라가기. ==> 최대 깊이 구하기. (dfs)..
+        makeDfs(board, aloc, bloc, 0);
+        return max-1;
 
+    }
 
-            //while(true){
-            // turns 홀짝 구하기 => aloc, bloc 번갈아서
-            //System.out.println("makeDfs 실행");
-            makeDfs(board, aloc, bloc, 1);
-            //}
+    public static void makeDfs(int[][] board, int[] aloc, int[] bloc, int turns){
 
 
 
-
-            return max;
+        if(board[aloc[0]][aloc[1]] == 0 || board[bloc[0]][bloc[1]] == 0){
+            max = Math.min(max, ++turns);
+            return;
         }
 
-        public static void makeDfs(int[][] board, int[] aloc, int[] bloc, int turns){
-            // turns가 홀수면 a // 짝수면 b
-            int[] newAloc = Arrays.copyOf(aloc, aloc.length);
-            int[] newBloc = Arrays.copyOf(bloc, bloc.length);
-            ArrayList<GameResult> al = new ArrayList<>();
-            if(turns % 2 != 0){
-                //loc = aloc;
+        turns++; //여기서 턴이 오르면 안 됨
+        // turns가 홀수면 a // 짝수면 b
 
-                al = moveResult(board, newAloc);  // loc 결과 판단 // 최대 4개의 결과 값.
-            }else{
+        int[] newAloc = Arrays.copyOf(aloc, aloc.length);
+        int[] newBloc = Arrays.copyOf(bloc, bloc.length);
+        ArrayList<GameResult> al = new ArrayList<>();
 
-                al = moveResult(board, newBloc);  // loc 결과 판단 // 최대 4개의 결과 값.
-            }
+        if(turns % 2 != 0){
 
-            // 여기서(재귀 건너서) 패배 할건지, moveResult에서 패배할건지
+            al = moveResult(board, newAloc, turns ,bloc);  // loc 결과 판단 // 최대 4개의 결과 값.
+        }else{
 
-
-
-
-            if(al.size() == 0){ // 결과값이 없으면 turn 저장
-                // System.out.println("moveResult결과 없음 ");
-                // System.out.println("al.size() == " + al.size());
-                // System.out.println();
-
-                //max = Math.max(max, turns);
-                return;
-            }else{
-                // 4개의 결과값에 대해 밑에 다 돌아야함. 보드상태, loc위치.  4개씩
-                // System.out.println("moveResult결과 있음 ");
-                // System.out.println("al.size() == " + al.size());
-                // System.out.println();
-                for(GameResult gr : al){
-
-                    if(turns % 2 != 0){
-                        newAloc = gr.loc;
-                        System.out.println("now turns == " + turns);
-                        System.out.println("aloc[0] == " + newAloc[0]);
-                        System.out.println("aloc[1] == " + newAloc[1]);
-                        max = Math.max(max, turns);
-                        System.out.println("max turns == " + max);
-                        System.out.println();
-                        // System.out.println("aloc = gr.loc 통과");
-                        //System.out.println("gr.board[0] == ")
-                    }else{
-                        newBloc = gr.loc;
-                        System.out.println("now turns == " + turns);
-                        System.out.println("bloc[0] == " + newBloc[0]);
-                        System.out.println("bloc[1] == " + newBloc[1]);
-                        max = Math.max(max, turns);
-                        System.out.println("max turns == " + max);
-                        System.out.println();
-                    }
-
-                    makeDfs(gr.board, newAloc, newBloc, ++turns);
-
-                }
-
-            }
-
-
-
-
-
-
-
-
-
+            al = moveResult(board, newBloc, turns, aloc);  // loc 결과 판단 // 최대 4개의 결과 값.
         }
 
-        public static ArrayList<GameResult> moveResult(int[][] board, int[] loc){
+        // 여기서(재귀 건너서) 패배 할건지, moveResult에서 패배할건지
 
-            // 4개의 상태를 반환해야함.
-            ArrayList<GameResult> al = new ArrayList<>();
+        if(al.size() == 0){
 
-            int [][] canMove = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-            for(int i = 0; i<canMove.length; i++){
+            max = Math.min(max, turns);
 
-                int newX = loc[0] + canMove[i][0];
-                int newY = loc[1] + canMove[i][1];
+            return;
+        }else{
 
-                if(0<= newX&& newX < board.length &&
-                        0<=newY && newY < board[0].length && board[newX][newY]  == 1){
-                    //성공
-                    //board[loc[0]][loc[1]] = 0; // 적용하면 안되지 ! // array copy
-                    //int[][] newBoard = System.arraycopy(board);
-                    int[][] newBoard = new int[board.length][board[0].length];
-                    for(int j = 0; j<board.length; j++){
-                        for(int k = 0; k<board[j].length; k++){
-                            newBoard[j][k] = board[j][k];
-                        }
-                    }
+            for(GameResult gr : al){
 
-                    newBoard[loc[0]][loc[1]] = 0;
+                if(turns % 2 != 0){ //a 턴일 때 b를 확인
 
-                    //loc[0] = newX;
-                    //loc[1] = newY;
-                    int[] newLoc = Arrays.copyOf(loc, loc.length);
-                    newLoc[0] = newX;
-                    newLoc[1] = newY;
-                    GameResult gr = new GameResult(newBoard, newLoc);
-                    al.add(gr);
+                    newAloc = gr.loc;
+
                 }else{
-                    //실패
 
+                    newBloc = gr.loc;
 
                 }
 
+                makeDfs(gr.board, newAloc, newBloc, turns);
+
             }
-            return al;
 
         }
 
+    }
 
-        public static class GameResult{
-            int[][] board;
-            int[] loc;
-            GameResult(int[][] board, int[] loc){
-                this.board = board;
-                this.loc = loc;
+    public static ArrayList<GameResult> moveResult(int[][] board, int[] loc, int turns, int[] opposit){
+        // 4개의 상태를 반환해야함.
+        ArrayList<GameResult> al = new ArrayList<>();
+
+        int [][] canMove = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        for(int i = 0; i<canMove.length; i++){
+
+            int newY= loc[0] + canMove[i][0];
+            int newX = loc[1] + canMove[i][1];
+
+            if(0<= newX&& newX < board[0].length &&  //x가 맞나..?
+                    0<=newY && newY < board.length && board[newY][newX]  == 1){ // y가 맞나 ..?
+
+                int[][] newBoard = new int[board.length][board[0].length];
+                for(int j = 0; j<board.length; j++){
+                    for(int k = 0; k<board[j].length; k++){
+                        newBoard[j][k] = board[j][k];
+                    }
+                }
+
+                newBoard[loc[0]][loc[1]] = 0;
+                int[] newLoc = {newY, newX};
+
+                GameResult gr = new GameResult(newBoard, newLoc);
+                al.add(gr);
+            }else{
+                //실패
             }
         }
+
+        if(al.size() > 2){ // 다른 방법이 있는 경우
+            for(int i = 0; i<al.size(); i++){
+                if(al.get(i).loc[0] == opposit[0] && al.get(i).loc[1] == opposit[1]){
+                    al.remove(i);
+                }
+            }
+        }
+
+        return al;
+
+    }
+
+
+    public static class GameResult{
+        int[][] board;
+        int[] loc;
+        GameResult(int[][] board, int[] loc){
+            this.board = board;
+            this.loc = loc;
+        }
+    }
 
 }
