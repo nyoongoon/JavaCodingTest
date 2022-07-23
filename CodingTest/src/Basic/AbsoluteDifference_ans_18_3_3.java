32a6661710763722c5be5a07e296d9186b5c79a9
nyoongoon93 <68639744+nyoongoon@users.noreply.github.com>
2022-07-23 10:41:13 +0900

    basic

---
+++ CodingTest/src/Basic/AbsoluteDifference_ans_18_3_3.java
@@ -0,0 +1,24 @@
+package Basic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class AbsoluteDifference_ans_18_3_3 {
+
+
+    public static void main(String[] args) {
+        // Test code
+        int[] arr = {1, 2, 3, 4, 5};
+        solution(arr, 4, 3);
+
+        arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        solution(arr, 5, 5);
+
+        arr = new int[]{2, 4};
+        solution(arr, 1, 3);
+
+        arr = new int[]{2, 4};
+        solution(arr, 3, 3);
+    }
+}
