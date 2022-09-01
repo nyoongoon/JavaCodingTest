490fc953498c88f3f5e6d848dc1db2598eabda6b
nyoongoon <nyoongoon@naver.com>
2022-09-02 08:44:10 +0900

    Basic

--- CodingTest/src/Basic/DequePalindrome_ans_11_2.java
+++ CodingTest/src/Basic/DequePalindrome_ans_11_2.java
@@ -1,2 +1,52 @@
-package Basic;public class DequePalindrome_ans_11_2 {
+package Basic;// Palindrome 찾기
+// Palindrome 이면 true, 아니면 false 를 반환하세요.
+// Palindrome: 앞으로 읽어도 거꾸로 읽어도 같은 문자열
+
+// 입출력 예시)
+// 입력: a
+// 결과: true
+
+// 입력: madam
+// 결과: true
+
+// 입력: abab
+// 결과: false
+
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.stream.IntStream;
+
+public class DequePalindrome_ans_11_2 {
+    public static boolean checkPalindrome(String str) {
+        Deque deque = new ArrayDeque();
+        boolean isFront = true;
+        boolean isPalindrome = true;
+
+        for(String s : str.split("")){
+            deque.addFirst(s);
+        }
+
+        while(!deque.isEmpty()){
+            String s1 = (String)deque.pollFirst(); // remove를 발생하면 비었을 시 에러
+            String s2 = (String)deque.pollLast(); // poll은 비었을 시 null
+
+            if(s1 != null && s2 != null && s1.equals(s2)){
+                isPalindrome = false;
+                break;
+            }
+        }
+        return isPalindrome;
+    }
+
+    public static void main(String[] args) {
+        // Test code
+        System.out.println(checkPalindrome("a"));       // true
+        System.out.println(checkPalindrome("aba"));     // true
+        System.out.println(checkPalindrome("abba"));    // true
+        System.out.println(checkPalindrome("abab"));    // false
+        System.out.println(checkPalindrome("abcba"));   // true
+        System.out.println(checkPalindrome("abccba"));  // true
+        System.out.println(checkPalindrome("madam"));  // true
+    }
 }
