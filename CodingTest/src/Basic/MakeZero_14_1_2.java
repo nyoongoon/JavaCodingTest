549989b6de8a0692d76ae700baa9a0ba1120593f
nyoongoon <nyoongoon@naver.com>
2022-09-26 08:33:51 +0900

    Basic

--- CodingTest/src/Basic/MakeZero_14_1_2.java
+++ CodingTest/src/Basic/MakeZero_14_1_2.java
@@ -1,26 +1,47 @@
 package Basic;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class MakeZero_14_1_2 {
+    static int cnt =0;
 
-    public static void solution(int[][] matrix) { //시간복잡도 O(n^3) => 답안풀이 참고
-        LinkedList<int[]> ll = new LinkedList<>();
+    public static void solution(int[][] matrix) {
+        HashMap<Integer, Integer> rowMap = new HashMap<>();
+        HashMap<Integer, Integer> colMap = new HashMap<>();
 
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if(matrix[i][j] == 0){
-                    ll.add(new int[]{i, j});
-                }
+            //col == 0 일때
+            if(matrix[i][0] == 0){
+                rowMap.put(i, 0);
+                //makeZero(matrix, i, 0);
+                continue;
+            }
+            rowMap.put(i, 1);
+        }
+
+        for(Integer row : rowMap.keySet()){
+            int rowNum = rowMap.get(row);
+            if(rowNum == 0){
+                makeZero(matrix, rowNum, 0);
             }
         }
 
-        for (int i = 0; i < ll.size(); i++) {
-            int[] result = ll.get(i);
-            if(matrix[result[0]][result[1]] == 0){
-                // matrix= makeZero(matrix, result[0], result[1]);
-                makeZero(matrix, result[0], result[1]);
+        for (int i = 0; i < matrix[0].length; i++) {
+            //row == 0 일때
+            if(matrix[0][i] == 0){
+                colMap.put(i, 0);
+                //makeZero(matrix, 0, i);
+                continue;
+            }
+            colMap.put(i, 1);
+        }
+
+        for(Integer col : colMap.keySet()){
+            int colNum = colMap.get(col);
+            if(colNum == 0){
+                makeZero(matrix, 0, colNum);
             }
         }
 
@@ -30,17 +51,57 @@ public class MakeZero_14_1_2 {
             }
             System.out.println();
         }
+
+        LinkedList<int[]> zeroVal = new LinkedList<>();
+
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if(matrix[i][j] == 0){
+                    // 체크
+                    //zeroVal.add(new int[] {i, j});
+                    if(rowMap.get(i) != 0){
+                        zeroVal.add(new int[] {i, j});
+                    }
+                    if(colMap.get(j) != 0){
+                        zeroVal.add(new int[] {i, j});
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < zeroVal.size(); i++) {
+            int row = zeroVal.get(i)[0];
+            int col = zeroVal.get(i)[1];
+            makeZero(matrix, row, col);
+        }
+
+        System.out.println();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("ctn == " + cnt);
+        System.out.println();System.out.println();System.out.println();
+
+
     }
 
-    public static int[][] makeZero(int[][] matrix, int row, int col){
+    public static void makeZero(int[][] matrix, int row, int col){
+        cnt++;
         for (int i = 0; i < matrix.length; i++) {
             matrix[row][i] = 0;
         }
+
         for (int i = 0; i < matrix[0].length; i++) {
             matrix[i][col] = 0;
         }
-        return matrix;
+
     }
+
+
 
     public static void main(String[] args) {
         // Test code
