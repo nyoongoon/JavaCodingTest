df5bfbd3633d59169ab262c6ec36d15ddc5f613d
nyoongoon <nyoongoon@naver.com>
2022-12-15 19:31:48 +0900

    HeapSort_re

--- CodingTest/src/Sort/HeapSort_re.java
+++ CodingTest/src/Sort/HeapSort_re.java
@@ -1,43 +1,43 @@
 package Sort;
 
 public class HeapSort_re {
-    static void heapSort(int[] arr) {
-        for (int i = arr.length / 2 - 1; i >= 0; i--) {
-            heapify(arr, 0, arr.length);
+
+    public static void heapSort(int [] arr){
+        for (int i = arr.length / 2 - 1; i >=0; i--) {
+            heapify(arr, i, arr.length);
         }
 
         for (int i = arr.length-1; i >=0 ; i--) {
-            swap(arr, i, 0);
+            swap(arr, 0, i);
             heapify(arr, 0, i);
         }
-        
     }
-    
-    static void heapify(int[] arr, int parentIdx, int size){
+
+    public static void heapify(int[] arr ,int parentIdx, int size){
         int maxIdx = parentIdx;
-        int lt = parentIdx * 2 + 1;
-        int rt = parentIdx * 2 + 2;
-        
-        if(lt < size && arr[maxIdx] < arr[lt]){
-            maxIdx = lt;
+        int left = parentIdx * 2 + 1;
+        int right = parentIdx * 2 + 2;
+
+        if(left < size && arr[maxIdx] < arr[left]){
+
+            maxIdx = left;
+        }
+        if(right < size && arr[maxIdx] < arr[right]){
+
+            maxIdx = right;
         }
 
-        if(rt < size && arr[maxIdx] < arr[rt]){
-            maxIdx = rt;
-        }
-        
         if(maxIdx != parentIdx){
-            swap(arr, maxIdx, parentIdx);
+            swap(arr, parentIdx, maxIdx);
             heapify(arr, maxIdx, size);
         }
     }
 
-    static void swap(int[] arr, int a, int b){
+    public static void swap(int[] arr, int a, int b){
         int tmp = arr[a];
         arr[a] = arr[b];
         arr[b] = tmp;
     }
-    
 
     public static void main(String[] args) {
         int[] arr = {3, 5, 2, 7, 1, 4, 6};
