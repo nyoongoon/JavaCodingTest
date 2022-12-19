8efbbc040048a2e800aacd0bb16d3bd24adeb207
nyoongoon <nyoongoon@naver.com>
2022-12-19 23:02:57 +0900

    QuickSort_re

--- CodingTest/src/Sort/QuickSort_re.java
+++ CodingTest/src/Sort/QuickSort_re.java
@@ -1,2 +1,44 @@
-package Sort;public class QuickSort_re {
+package Sort;
+
+import java.util.Arrays;
+
+public class QuickSort_re {
+    static void quickSort(int[] arr, int left, int right){
+        if(left >= right) return;
+
+        int pivot = partition(arr, left, right);
+        quickSort(arr, left, pivot-1);
+        quickSort(arr, pivot+1, right);
+
+    }
+
+    static int partition(int[] arr, int left, int right){
+        int pivotVal = arr[left];
+        int lt = left + 1;
+        int rt = right;
+
+        while(lt < rt){
+            while(pivotVal < arr[rt] && lt < rt){
+                rt--;
+            }
+            while(pivotVal >= arr[lt] && lt < rt){
+                lt++;
+            }
+            swap(arr, lt, rt);
+        }
+        swap(arr, left, lt);
+        return lt;
+    }
+
+    static void swap(int[] arr, int a, int b){
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {6, 2, 7, 9, 4, 5, 8};
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println("퀵 정렬: " + Arrays.toString(arr));
+    }
 }
