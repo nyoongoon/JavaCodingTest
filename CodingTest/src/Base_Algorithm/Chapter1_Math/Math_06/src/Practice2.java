c15d7526b993cee4bdb43fd043ff58ed63f0d7ca
nyoongoon <nyoongoon@naver.com>
2023-02-24 19:58:11 +0900

    최대공약수 재귀로 구하기 정답풀이

--- CodingTest/src/Base_Algorithm/Chapter1_Math/Math_06/src/Practice2.java
+++ CodingTest/src/Base_Algorithm/Chapter1_Math/Math_06/src/Practice2.java
@@ -2,21 +2,11 @@ package Base_Algorithm.Chapter1_Math.Math_06.src;// Practice2
 // 최대공약수를 재귀함수로 구현하시오.
 
 public class Practice2 {
-    public static int gcd(int a, int b){
-        int max = Math.max(a, b);
-        int min = Math.min(a, b);
-        if(max % min == 0){
-            return min;
+    public static int gcd(int a, int b) { // ...??
+        if (a % b == 0) {
+            return b;
         }
-
-        for (int i = min - 1; i > 0; i--) {
-            if(min % i == 0){
-                min = i;
-                break;
-            }
-        }
-
-        return gcd(min, max);
+        return gcd(b, a % b);
     }
 
     public static void main(String[] args) {
