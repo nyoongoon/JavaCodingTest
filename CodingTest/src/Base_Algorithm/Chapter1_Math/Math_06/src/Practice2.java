23b9d98a2363b461c380afb32ed3c62313d09f60
nyoongoon <nyoongoon@naver.com>
2023-02-28 19:46:36 +0900

    pascal tri

--- CodingTest/src/Base_Algorithm/Chapter1_Math/Math_06/src/Practice2.java
+++ CodingTest/src/Base_Algorithm/Chapter1_Math/Math_06/src/Practice2.java
@@ -2,7 +2,9 @@ package Base_Algorithm.Chapter1_Math.Math_06.src;// Practice2
 // 최대공약수를 재귀함수로 구현하시오.
 
 public class Practice2 {
-    public static int gcd(int a, int b) { // ...??
+    //https://ko.wikipedia.org/wiki/%EC%9C%A0%ED%81%B4%EB%A6%AC%EB%93%9C_%ED%98%B8%EC%A0%9C%EB%B2%95
+    //https://www.youtube.com/watch?v=rb71JCx6Hb0 //유니온티비 증명 보기...
+    public static int gcd(int a, int b) { // ...?? -> 유클리드 호제법
         if (a % b == 0) {
             return b;
         }
