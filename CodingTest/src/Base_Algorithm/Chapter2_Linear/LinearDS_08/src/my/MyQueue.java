e69693802abba6751ffb538aad5715584f785572
nyoongoon <nyoongoon@naver.com>
2023-08-30 19:49:06 +0900

    원형 큐 구현 & 큐 문제

--- CodingTest/src/Base_Algorithm/Chapter2_Linear/LinearDS_08/src/my/MyQueue.java
+++ CodingTest/src/Base_Algorithm/Chapter2_Linear/LinearDS_08/src/my/MyQueue.java
@@ -1,67 +1,60 @@
-package Base_Algorithm.Chapter2_Linear.LinearDS_08.src;// Practice2
-// 배열을 이용한 기본 큐 직접 구현 (원형 큐)
+package Base_Algorithm.Chapter2_Linear.LinearDS_08.src.my;
 
-class MyQueue2 {
+class MyQueue {
     int[] arr;
-    int front;
+    int front; //배열로 만든 원형큐에서 front는 항상 비워둔다
     int rear;
 
-    MyQueue2(int size) {
-        arr = new int[size + 1];
+    MyQueue(int size) {
+        arr = new int[size + 1]; //front를 비워두므로 길이 + 1
         front = 0;
         rear = 0;
     }
 
     public boolean isEmpty() {
-       if(front == rear){
-           return true;
-       }
-       return false;
-    }
-
-    //
-    public boolean isFull() {
-        if((rear + 1) % arr.length == front){
+        if (front == rear) {
             return true;
         }
         return false;
     }
 
+    public boolean isFull() {
+        if ((rear + 1) % arr.length == front) {
+            return true;
+        }
+        return false;
+    }
 
     public void enqueue(int data) {
-        if(isFull()){
+        if (isFull()) {
             System.out.println("Queue is full ...");
             return;
         }
-
-        arr[++rear%arr.length] = data;
-
+        rear = (rear + 1) % arr.length;
+        arr[rear] = data;
     }
 
-
     public Integer dequeue() {
-        if(isEmpty()){
-            System.out.println("Queue is Empty ,,,");
+        if (isEmpty()) {
+            System.out.println("Queue is empty ...");
             return null;
         }
-
-        return arr[++front%arr.length];
+        front = (front + 1) % arr.length;
+        return arr[front];
     }
 
     public void printQueue() {
-        int start = (front+1)%arr.length;
-        int end = (rear+1)%arr.length;
-        for (int i = start; i < end; i = (i+1)%arr.length) {
-            System.out.print(arr[i] + ", ");
+        int start = (front + 1) % arr.length;
+        int end = (rear + 1) % arr.length; //rear는 자체에 마지막 값이 있으므로 rear+1 % arr.length은 출력하지 않는다.
+        for (int i = start; i != end; i = (i + 1) % arr.length) { // 조건문 != end 주의!!!
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
-}
 
-public class Practice2 {
     public static void main(String[] args) {
         // Test code
-        MyQueue2 myQueue = new MyQueue2(5);
+        MyQueue myQueue = new MyQueue(5);
         myQueue.enqueue(1);
         myQueue.enqueue(2);
         myQueue.enqueue(3);
