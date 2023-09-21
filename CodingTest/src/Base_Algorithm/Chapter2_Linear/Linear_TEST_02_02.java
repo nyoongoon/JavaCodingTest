9d4420c4a420a966cc432e7eba9eeb647dcb7119
nyoongoon <nyoongoon@naver.com>
2023-09-21 19:38:27 +0900

    배열-큰수의덧셈-정답

--- CodingTest/src/Base_Algorithm/Chapter2_Linear/Linear_TEST_02_02.java
+++ CodingTest/src/Base_Algorithm/Chapter2_Linear/Linear_TEST_02_02.java
@@ -1,10 +1,5 @@
 package Base_Algorithm.Chapter2_Linear;
 
-import java.util.Arrays;
-
-import static Base_Algorithm.FileUtils.*;
-import static java.util.Arrays.*;
-
 public class Linear_TEST_02_02 {
 
     public static void main(String[] args) {
@@ -15,9 +10,33 @@ public class Linear_TEST_02_02 {
         for (int i = 0; i < result.length; i++) {
             System.out.print(result[i] + " ");
         }
+        System.out.println();
+        int[] a1 = {8, 2, 6, 3, 4, 1, 5, 7, 2, 8};
+        int[] b1 = {0}; //[1, 0, 2, 6, 9, 7, 6, 1, 0, 9, 5]
+        int[] result1 = solution(a1, b1);
+
+        for (int i = 0; i < result1.length; i++) {
+            System.out.print(result1[i] + " ");
+        }
+        System.out.println();
+        int[] a2 = {8, 2, 6, 3, 4, 1, 5, 7, 2, 8};
+        int[] b2 = {}; //[1, 0, 2, 6, 9, 7, 6, 1, 0, 9, 5]
+        int[] result2 = solution(a2, b2);
+
+        for (int i = 0; i < result2.length; i++) {
+            System.out.print(result2[i] + " ");
+        }
+        System.out.println();
     }
 
     public static int[] solution(int[] a, int[] b) {
+
+        if (a.length == 0) {
+            return b;
+        }
+        if (b.length == 0) {
+            return a;
+        }
 
         int size;
         // ㅂㅐ열 길이가 증가하는 경우 체크
@@ -43,7 +62,12 @@ public class Linear_TEST_02_02 {
 
 
             result[resultIdx] += (aValue + bValue) % 10;
-            result[resultIdx - 1] += (resultIdx != 0 && aValue + bValue >= 10) ? 1 : 0;
+//            System.out.println("resultIdx  " + resultIdx);
+//            System.out.println("aIdx  " + aIdx);
+//            System.out.println("bIdx  " + bIdx);
+            if (resultIdx - 1 >= 0) {
+                result[resultIdx - 1] += (resultIdx != 0 && aValue + bValue >= 10) ? 1 : 0;
+            }
 
             aIdx--;
             bIdx--;
