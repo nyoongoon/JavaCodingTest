8cbd627d1cb3304516e2d8eb776f3d051c2d13f5
nyoongoon <nyoongoon@naver.com>
2023-11-08 19:47:43 +0900

    뱀게임 정답 풀이

---
+++ CodingTest/src/Base_Algorithm/Chapter2_Linear/LinearDS_14_1/src/Practice4_ans.java
@@ -0,0 +1,48 @@
+package Base_Algorithm.Chapter2_Linear.LinearDS_14_1.src;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Practice4_ans {
+    public static void solution(String str) {
+        Stack<String> stk = new Stack<>();
+        Map<String, String> map = new HashMap<>();
+        map.put("(", ")");
+        map.put("[", "]");
+        map.put("{", "}");
+
+        for(String s : str.split("")){
+            if(map.containsKey(s)){
+                stk.push(s);
+                continue;
+            }
+            if(map.containsValue(s) && !map.get(stk.peek()).equals(s)){
+                System.out.println("FAIL");
+                return;
+            }
+            if(map.containsValue(s) && map.get(stk.peek()).equals(s)){
+                stk.pop();
+                continue;
+            }
+        }
+
+        if (stk.size() == 0){
+            System.out.println("PASS");
+            return;
+        }
+
+        System.out.println("FAIL");
+    }
+
+
+
+    public static void main(String[] args) {
+        // Test code
+        solution("[yyyy]-[mm]-[dd]");               // Pass
+        solution("System.out.println(arr[0][1))");  // FAIL
+        solution("Support [Java or Python(3.x)]");  // PASS
+        solution("([[{}])");                        // FAIL
+    }
+}
