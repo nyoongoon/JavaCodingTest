8ca4896c9291c40391a0ef1cf10e8d69594aedfb
nyoongoon <nyoongoon@naver.com>
2023-11-10 19:47:52 +0900

    선형자료구조 문제풀이 14-1, 14-2

--- CodingTest/src/Base_Algorithm/Chapter2_Linear/LinearDS_14_2/src/Practice1.java
+++ CodingTest/src/Base_Algorithm/Chapter2_Linear/LinearDS_14_2/src/Practice1.java
@@ -1,7 +1,41 @@
 package Base_Algorithm.Chapter2_Linear.LinearDS_14_2.src;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
 public class Practice1 {
     public static void solution(int docs, int target, int[] priority) {
+        ArrayList<Doc> list  = new ArrayList<>();
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int i = 0; i < priority.length; i++) {
+            Doc doc = new Doc(priority[i], i); // priority
+            list.add(doc);
+            priorityQueue.add(priority[i]);
+        }
+
+
+        int order = 0;
+        int i = 0;
+        while (!priorityQueue.isEmpty()) {
+            int topPriority = priorityQueue.peek();
+            Doc doc = list.get(i);
+            if (list.get(i).prioriy >= topPriority) {
+                order++;
+                priorityQueue.poll();
+                list.remove(i);
+
+                if(doc.idx == target){
+                    System.out.println(order);
+                    return;
+                }
+            } else{
+                list.add(list.remove(i));
+                i = (i + 1) & list.size();
+            }
+        }
+
 
     }
 
@@ -21,5 +55,26 @@ public class Practice1 {
         target = 0;
         priority = new int[]{1, 1, 9, 1, 1, 1};
         solution(docs, target, priority);
+    }
+
+    static class Doc implements Comparable<Doc> {
+        int prioriy;
+        int idx;
+
+        Doc(int prioriy, int idx) {
+            this.prioriy = prioriy;
+            this.idx = idx;
+        }
+
+        @Override
+        public int compareTo(Doc doc) {
+            if (this.prioriy > doc.prioriy) {
+                return -1;
+            } else if (this.prioriy < doc.prioriy) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
     }
 }
