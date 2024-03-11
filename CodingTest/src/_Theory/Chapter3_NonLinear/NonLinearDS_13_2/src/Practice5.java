dde67fe354738b7ed2bc71f57e0cf4f0d515d86d
nyoongoon <nyoongoon@naver.com>
2024-03-11 20:15:45 +0900

    비선형자료구조-문제풀이5-트라이변형

--- CodingTest/src/_Theory/Chapter3_NonLinear/NonLinearDS_13_2/src/Practice5.java
+++ CodingTest/src/_Theory/Chapter3_NonLinear/NonLinearDS_13_2/src/Practice5.java
@@ -2,66 +2,21 @@ package _Theory.Chapter3_NonLinear.NonLinearDS_13_2.src;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-class Node {
-    HashMap<Character, Node> child;
-    boolean isTerminal;
-
-    public Node() {
-        this.child = new HashMap<>();
-        this.isTerminal = false;
-    }
-}
-
-class Trie {
-    Node root;
-
-    public Trie() {
-        this.root = new Node();
-    }
-
-    public void insert(String str) {
-        Node cur = this.root;
-
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-
-            cur.child.putIfAbsent(c, new Node());
-            cur = cur.child.get(c);
-
-            if (i == str.length() - 1) {
-                cur.isTerminal = true;
-                return;
-            }
-        }
-    }
-
-    public boolean search(String str) {
-        Node cur = this.root;
-
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-
-            if (cur.child.containsKey(c)) {
-                cur = cur.child.get(c);
-            } else {
-                return false;
-            }
-
-            if (i == str.length() - 1) {
-                if (!cur.isTerminal) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-}
 
 public class Practice5 {
     public static ArrayList<Boolean> solution(String[] queries, String pattern) {
+        ArrayList<Boolean> result = new ArrayList<>();
+        Trie trie = new Trie();
+        trie.add(pattern);
 
-        return null;
+        for (String str : queries) {
+            result.add(trie.search(str));
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
@@ -75,5 +30,62 @@ public class Practice5 {
 
         pattern = "FoBaT";
         System.out.println(solution(queries, pattern));
+    }
+
+    public static class Node {
+        Character value;
+        boolean isTerminal;
+        Map<Character, Node> child;
+
+        public Node(Character value, boolean isTerminal) {
+            this.value = value;
+            this.isTerminal = isTerminal;
+            this.child = new HashMap<>();
+        }
+    }
+
+    public static class Trie {
+        Node root;
+
+        public Trie() {
+            this.root = new Node(null, false);
+        }
+
+        public void add(String str) {
+            Node cur = this.root;
+
+            for (int i = 0; i < str.length() - 1; i++) {
+                char c = str.charAt(i);
+                if (!cur.child.containsKey(c)) {
+                    cur.child.put(c, new Node(c, false));
+                }
+                cur = cur.child.get(c);
+            }
+
+            char lastC = str.charAt(str.length() - 1);
+            cur.child.put(lastC, new Node(lastC, true));
+        }
+
+        public boolean search(String str) {
+            Node cur = this.root;
+
+            // 맨처음은 무조건 패턴..?
+            for (int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
+                if(Character.isUpperCase(c) && !cur.child.containsKey(c)){
+                    return false;
+                }
+
+                if (cur.child.containsKey(c)) {
+                    cur = cur.child.get(c);
+                }
+            }
+
+            if (cur.isTerminal) {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
