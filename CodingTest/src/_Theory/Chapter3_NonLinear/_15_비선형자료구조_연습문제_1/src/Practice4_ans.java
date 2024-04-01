08e803ef1ef0f5554617b78c677934b6fefd119a
nyoongoon <nyoongoon@naver.com>
2024-04-01 19:05:50 +0900

    bfs 연습문제

--- CodingTest/src/_Theory/Chapter3_NonLinear/_15_비선형자료구조_연습문제_1/src/Practice4_ans.java
+++ CodingTest/src/_Theory/Chapter3_NonLinear/_15_비선형자료구조_연습문제_1/src/Practice4_ans.java
@@ -2,10 +2,48 @@ package _Theory.Chapter3_NonLinear._15_비선형자료구조_연습문제_1.src;
 
 import java.util.*;
 
-public class Practice4_ans {
+public class Practice4_ans {//TODO 여기서부터 문제풀이 보기..!
 
     public static int solution(String[] deadends, String target) {
-        return 0;
+        int cnt = 0;
+        Set<String> visited = new HashSet<>(Arrays.asList(deadends));
+        Queue<String> queue = new LinkedList<>();
+        queue.add("0000");
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String curNum = queue.poll();
+//                System.out.println(curNum);
+                if (!visited.add(curNum)) {
+                    continue;
+                }
+                if (curNum.equals(target)) {
+                    return cnt;
+                }
+                for (String nextNums : getNextNums(curNum.toCharArray())) {
+                    if (!visited.contains(nextNums)) {
+                        queue.add(nextNums);
+                    }
+                }
+            }
+            cnt++;
+        }
+
+        return -1;
+    }
+
+    private static List<String> getNextNums(char[] cArr) {
+        List<String> nums = new ArrayList<>();
+        for (int i = 0; i < cArr.length; i++) {
+            char c = cArr[i];
+            cArr[i] = c == '9' ? '0' : (char) (c + (char) 1);
+            nums.add(String.valueOf(cArr));
+            cArr[i] = c == '0' ? '9' : (char) (c - (char) 1);
+            nums.add(String.valueOf(cArr));
+            cArr[i] = c;
+        }
+        return nums;
     }
 
 
@@ -19,13 +57,4 @@ public class Practice4_ans {
 
     }
 
-    public static class Lock {
-        int[] arr;
-        int moves;
-
-        public Lock(int[] arr, int moves) {
-            this.arr = arr;
-            this.moves = moves;
-        }
-    }
 }
