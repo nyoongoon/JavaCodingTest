100fbae9b275df9c43461958ca3df61181b3e6bf
nyoongoon <nyoongoon@naver.com>
2024-04-18 19:44:54 +0900

    bfs 연습문제

--- CodingTest/src/_Theory/Chapter3_NonLinear/_16_비선형자료구조_연습문제_2/src/Practice3.java
+++ CodingTest/src/_Theory/Chapter3_NonLinear/_16_비선형자료구조_연습문제_2/src/Practice3.java
@@ -3,45 +3,53 @@ package _Theory.Chapter3_NonLinear._16_비선형자료구조_연습문제_2.src;
 import java.util.Arrays;
 
 public class Practice3 {
-    static int maxSum = 0;
+    static int maxValue = Integer.MIN_VALUE;
 
     public static boolean solution(int[] target) {
-        maxSum = Arrays.stream(target).max().orElseThrow();
-
-        int[] arr = new int[target.length];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = 1;
+        for (int i = 0; i < target.length; i++) {
+            maxValue = Math.max(maxValue, target[i]);
         }
 
-        return dfs(target, arr);
+        int[] array = new int[target.length];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = 1;
+        }
+
+        return dfs(array, target);
     }
 
-    private static boolean dfs(int[] target, int[] arr) {
-        if(isEqual(target, arr)) {
+    public static boolean dfs(int[] array, int[] target) {
+        if (isEqualArray(array, target)) {
             return true;
         }
-        if(Arrays.stream(arr).sum() > maxSum){
+
+        int sum = Arrays.stream(array).sum();
+        if (sum > maxValue) {
             return false;
         }
 
-        int sum = Arrays.stream(arr).sum();
-        for (int i = 0; i < arr.length; i++) {
-            int tmp = arr[i];
-            arr[i] = sum;
-            if(dfs(target, arr)){
+        for (int i = 0; i < array.length; i++) {
+            int originValue = array[i];
+            array[i] = sum;
+            if (dfs(array, target)) {
                 return true;
             }
-            arr[i] = tmp;
+            array[i] = originValue;
         }
+
         return false;
     }
 
-    private static boolean isEqual(int[] target, int[] arr) {
-        if(target.length != arr.length){
-            return false;
+    private static void print(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]+" ");
         }
-        for (int i = 0; i < target.length; i++) {
-            if(target[i] != arr[i]){
+        System.out.println();
+    }
+
+    private static boolean isEqualArray(int[] array, int[] target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != target[i]) {
                 return false;
             }
         }
---
+++ CodingTest/src/_Theory/Chapter3_NonLinear/_16_비선형자료구조_연습문제_2/src/Practice3_ans.java
@@ -0,0 +1,51 @@
+package _Theory.Chapter3_NonLinear._16_비선형자료구조_연습문제_2.src;
+
+import java.util.PriorityQueue;
+
+/**
+ * 어려움...
+ */
+public class Practice3_ans {
+
+    public static boolean solution(int[] target) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((x, y) -> y - x);
+        int sum = 0;
+        for (int i = 0; i < target.length; i++) {
+            sum += target[i];
+            pq.add(i);
+        }
+
+
+        //sum == 17
+        while (pq.peek() != 1) {
+            int num = pq.poll(); //9
+            sum -= num; // 8
+
+            // 값 체크가 이상한거 같음.. -> num <= sum 으로 충분한거 아닌가.?
+            if(num <= sum || sum < 1){ //sum == 0이면 마이너스 전에 값이 같은 것.
+                return false;
+            }
+
+            // 값 체크가 이상한거 같음.. -> 위에서 num <= 했으므로 결과는 항상 num > 0 아닌가..?
+            num -= sum; // num == 1
+            sum += num; // 새로 생성된 숫자만 더하면 sum임
+            pq.add(num > 0 ? num : sum); // ????
+
+        }
+
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        // Test code
+        int[] target = {9, 3, 5};
+        System.out.println(solution(target));
+
+        target = new int[]{8, 5};
+        System.out.println(solution(target));
+
+        target = new int[]{1, 1, 1, 2};
+        System.out.println(solution(target));
+    }
+}
