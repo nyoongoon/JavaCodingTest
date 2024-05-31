d49804e6c468f80ef1b355a31fdb0b8af64c842c
nyoongoon <nyoongoon@naver.com>
2024-05-31 19:30:38 +0900

    정렬 연습문제

--- CodingTest/src/_Theory/Chapter4_Algorithm/_04_정렬_연습문제/src/ans/Practice4_re.java
+++ CodingTest/src/_Theory/Chapter4_Algorithm/_04_정렬_연습문제/src/ans/Practice4_re.java
@@ -1,4 +1,4 @@
-package _Theory.Chapter4_Algorithm._04_정렬_연습문제.src;// Practice4
+package _Theory.Chapter4_Algorithm._04_정렬_연습문제.src.ans;// Practice4
 // 정수 배열 nums 가 주어졌을 때
 // 오름차순으로 정렬하기 위해 배열 내에서 정렬이 필요한 구간의 길이를 출력하는 프로그램을 작성하세요.
 
@@ -8,11 +8,11 @@ package _Theory.Chapter4_Algorithm._04_정렬_연습문제.src;// Practice4
 
 // 입력: 1, 3, 1
 // 출력: 2
-
-public class Practice4 {
-    public static int solution(int[] nums) { //todo 틀림!!! (예외 상황 있음..)
-        int startIdx = 0; //nums[startIdx] 보다 크면서 이후로 작은 값이 없는 것이 endIdx..
-        int endIdx = 0;
+// 틀렸음, 더 좋게 푸는 방법 있음..
+public class Practice4_re {
+    public static int solution(int[] nums) { //틀려서 다시 품.. -> startIdx의 값보다 큰 것이 아니라 중간에 가장 큰 값..
+        int startIdx = 0;
+        int endIdx = 0; // max 보다 크면서 이후로 작은 값이 없는 것이 endIdx..
 
         startIdx = getStartIdx(nums);
 
@@ -22,10 +22,7 @@ public class Practice4 {
 
         endIdx = getEndIdx(nums, startIdx);
 
-//        System.out.println("startIdx == " + startIdx);
-//        System.out.println("endIdx == " + endIdx);
-
-        return endIdx - startIdx + 1; // 2 조건만 만족시킴.. --> 1조건에서 마지막이 아니라 마지막-1 (작<큰) 의 작을 선택해야함..
+        return endIdx - startIdx + 1;
     }
 
     public static int getStartIdx(int[] nums) {
@@ -41,18 +38,31 @@ public class Practice4 {
     }
 
     public static int getEndIdx(int[] nums, int startIdx) {
+        int max = nums[startIdx];
         int endIdx = nums.length - 1;
 
         for (int i = startIdx + 1; i < nums.length; i++) {
-            if (nums[i - 1] < nums[i] && nums[startIdx] < nums[i]) {
+            if (nums[i - 1] < nums[i] && max < nums[i]) {
                 if (isOrdered(nums, i)) {
+                    System.out.println("last max == " + max); // --> 1번 문제의 8이 나와야 함..
                     endIdx = i - 1;
                     break;
+                }else{
+                    max = Math.max(max, nums[i]);
                 }
             }
         }
 
         return endIdx;
+    }
+
+    public static boolean isOrdered(int[] nums, int idx) {
+        for (int i = idx; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
@@ -68,14 +78,5 @@ public class Practice4 {
 
         nums = new int[]{1, 2, 3, 4, 5};
         System.out.println(solution(nums));
-    }
-
-    public static boolean isOrdered(int[] nums, int idx) {
-        for (int i = idx; i < nums.length - 1; i++) {
-            if (nums[i] > nums[i + 1]) {
-                return false;
-            }
-        }
-        return true;
     }
 }
