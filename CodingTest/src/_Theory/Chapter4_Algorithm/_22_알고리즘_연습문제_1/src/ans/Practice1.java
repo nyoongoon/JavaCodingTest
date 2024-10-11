6b4e98fe7bd548fcd4ce9e3a5c3532e44ff0fd3b
nyoongoon <nyoongoon@naver.com>
2024-10-11 19:29:09 +0900

    투포인터 연습문제

---
+++ CodingTest/src/_Theory/Chapter4_Algorithm/_22_알고리즘_연습문제_1/src/ans/Practice1.java
@@ -0,0 +1,16 @@
+package _Theory.Chapter4_Algorithm._22_알고리즘_연습문제_1.src.ans;
+
+
+public class Practice1 {
+    public static int solution(int n, int[] times) {
+       return -1;
+    }
+
+
+    public static void main(String[] args) {
+        // Test code
+        int n = 6;
+        int[] times = {7, 10};
+        System.out.println(solution(n, times));
+    }
+}
--- CodingTest/src/_Theory/Chapter4_Algorithm/_22_알고리즘_연습문제_1/src/ans/Practice2.java
+++ CodingTest/src/_Theory/Chapter4_Algorithm/_22_알고리즘_연습문제_1/src/ans/Practice2.java
@@ -1,55 +1,58 @@
 package _Theory.Chapter4_Algorithm._22_알고리즘_연습문제_1.src.ans;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Practice2 {
     public static int solution(int n, int[] plates, int types, int coupon) {
         if (plates == null || plates.length == 0) {
             return -1;
         }
-        int max = Integer.MIN_VALUE;
-
-        Map<Integer, Integer> map = new HashMap<>();
-        // 투포인터 만들기
+        int[] selected = new int[types + 1];
         int p1 = 0;
         int p2 = p1 + n - 1;
+
+        int max = Integer.MIN_VALUE;
+        int cnt = 0;
+
         for (int i = p1; i <= p2; i++) {
-            Integer value = map.getOrDefault(plates[i], 0);
-            map.put(plates[i], value + 1);
+            if (selected[i] == 0) {
+                cnt++;
+            }
+            selected[i] += 1;
         }
 
-        max = getMax(map, max, coupon);
+        max = getMax(coupon, selected, max, cnt);
 
         for (int i = 1; i < plates.length; i++) {
-            // 이전 것 빼기
-            Integer p1Value = map.getOrDefault(plates[p1], 1);
-            if(p1Value - 1 <= 0){
-                map.remove(plates[p1]);
-            }else{
-                map.put(plates[p1], p1Value - 1);
+            p1 = i;
+            p2 = (p1 + n - 1) % plates.length;
+
+            //p1-1에 -1 하기
+            selected[p1 - 1] -= 1;
+            if (selected[p1 - 1] == 0) {
+                cnt--;
             }
 
-            p1++;
-            p2 = (p2 + 1) % plates.length;
+            //p2에 +1 하기
+            if (selected[p2] == 0) {
+                cnt++;
+            }
+            selected[p2] += 1;
 
-            Integer p2Value = map.getOrDefault(plates[p2], 0);
-            map.put(plates[p2], p2Value + 1);
-
-            max = getMax(map, max, coupon);
+            max = getMax(coupon, selected, max, cnt);
         }
+
 
         return max;
     }
 
-    private static int getMax(Map<Integer, Integer> map, int max, int coupon) {
-        if(map.containsKey(coupon)){
-            max = Math.max(max, map.size());
-        }else{
-            max = Math.max(max, map.size() + 1);
+    private static int getMax(int coupon, int[] selected, int max, int cnt) {
+        if (selected[coupon] == 0) {
+            max = Math.max(max, cnt + 1);
+        } else {
+            max = Math.max(max, cnt);
         }
         return max;
     }
+
 
     public static void main(String[] args) {
         // Test code
