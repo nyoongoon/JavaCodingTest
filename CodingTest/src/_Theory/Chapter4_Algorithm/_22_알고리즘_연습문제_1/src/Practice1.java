5d4ab160f1972725b81af11792249a007e4f32ca
nyoongoon <nyoongoon@naver.com>
2024-10-15 20:05:25 +0900

    그리디 연습문제

--- CodingTest/src/_Theory/Chapter4_Algorithm/_22_알고리즘_연습문제_1/src/Practice1.java
+++ CodingTest/src/_Theory/Chapter4_Algorithm/_22_알고리즘_연습문제_1/src/Practice1.java
@@ -1,12 +1,34 @@
 package _Theory.Chapter4_Algorithm._22_알고리즘_연습문제_1.src;
 
 
-
 import java.util.Arrays;
 
 public class Practice1 {
+
     public static int solution(int n, int[] times) {
-       return -1;
+        int p1 = 0;
+        int max = Arrays.stream(times).max().getAsInt();
+        int p2 = n * max;
+
+
+        while (p1 < p2) {
+            int result = 0;
+            int mid = (p1 + p2) / 2;
+
+            for (int i = 0; i < times.length; i++) {
+                result += mid / times[i];
+            }
+
+            if (result < n) { // 시간이 부족
+                p1 = mid + 1;
+            } else if (result > n) {
+                p2 = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
     }
 
 
--- CodingTest/src/_Theory/Chapter4_Algorithm/_22_알고리즘_연습문제_1/src/Practice3.java
+++ CodingTest/src/_Theory/Chapter4_Algorithm/_22_알고리즘_연습문제_1/src/Practice3.java
@@ -1,13 +1,59 @@
 package _Theory.Chapter4_Algorithm._22_알고리즘_연습문제_1.src;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Practice3 {
+
 
     public static int solution(int n, int[] items) {
         if (items == null || items.length == 0) {
             return -1;
         }
 
-        return 0;
+        boolean[] used = new boolean[items.length + 1];
+
+        int idx = 0;
+        int cnt = 0;
+        // 초기상태 업데이트
+        while (cnt < n) {
+            if (!used[items[idx]]) {
+                used[items[idx]] = true;
+                cnt++;
+            }
+            idx++;
+        }
+        int result = 0;
+        while (idx < items.length) {
+            //현재 위치의 전기제품이 꽂혀있지 않은 경우
+            if (!used[items[idx]]) {
+                ArrayList<Integer> list = new ArrayList<>();
+                // 현재 꽂혀 있는 것 중에 나중에 다시 나오는 것은 빼지 않기
+                for (int i = idx; i < items.length; i++) {
+                    if (used[items[i]] && !list.contains(items[i])) {
+                        list.add(items[i]);
+                    }
+                }
+                // 현재 꽂혀있는 것이 나중에 다 사용되는 경우
+                if (list.size() == n) {
+                    // 그리디하게 가장 마지막에 사용될 것을 콘센트에서 뽑고 현재 것을 꼽기
+                    used[list.get(n - 1)] = false;
+                } else {
+                    // 안쓰는 것이 있는 경우
+                    for (int i = 1; i < items.length; i++) {
+                        //현재 사용중인데 리스트에 없는 것
+                        if (used[i] && !list.contains(i)) { //현재는 꽂혀 있지만 나중에 사용하게되지 않을 제품
+                            used[i] = false;
+                            break;
+                        }
+                    }
+                }
+                used[items[idx]] = true;
+                result++;
+            }
+            idx++;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
@@ -16,4 +62,5 @@ public class Practice3 {
         int[] items = {2, 3, 2, 3, 1, 2, 7};
         System.out.println(solution(n, items));
     }
+
 }
