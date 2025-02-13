a8eac3aef44910b46073ea273793758734923ac8
nyoongoon <nyoongoon@naver.com>
2025-02-13 21:11:25 +0900

    삼각 달팽이 내풀이

--- CodingTest/src/_3_프로그래머스_코딩_테스트_문제_풀이_전략_자바편/_1_배열/_1_교점에_별_만들기_책풀이.java
+++ CodingTest/src/_3_프로그래머스_코딩_테스트_문제_풀이_전략_자바편/_1_배열/_1_교점에_별_만들기_책풀이.java
@@ -6,7 +6,8 @@ import java.util.*;
  * 암기 포인트
  * 1. Set 에서의 동등 비교 -> equals & hashCode
  * 2. 나누어 떨어지지 않는 수 비교 -> 분모 분자 각각 체크 (나눠보기 전에) //주로 틀린 곳
- * 3. 큰 수 long 처리
+ * 3. 큰 수 long 처리 -> long denominator = (long)a*d - (long)b*c; //이런경우 (long)(a*d)하면 틀림 주의! --> (long)a * d에서 a가 long으로 변환되면, d도 자동으로 long으로 변환된 후 곱셈이 수행됩니다.
+ * 4. 배열 <-> 좌표 변환 공식 암기 --> x = x - minX, y = maxY - y;
  */
 public class _1_교점에_별_만들기_책풀이 {
     public static void main(String[] args) {
@@ -24,16 +25,6 @@ public class _1_교점에_별_만들기_책풀이 {
 
 
     class Solution {
-        class Point{
-            long x;
-            long y;
-            Point(long x, long y){
-                this.x = x;
-                this.y = y;
-            }
-        }
-
-
         public String[] solution(int[][] line) {
             List<Point> list = getPoints(line);
 
@@ -47,50 +38,71 @@ public class _1_교점에_별_만들기_책풀이 {
             // System.out.println("width == " + width);
 
             char[][] chars = new char[hight][width];
-            for(char[] row : chars){
+            for (char[] row : chars) {
                 Arrays.fill(row, '.');
             }
-
-            for(Point p : list){
+            /**
+             * Y 좌표 변환
+             * java
+             * 복사
+             * 편집
+             * int y = (int) (maxPoint.y - p.y);
+             * 여기서는 maxPoint.y에서 현재 p.y 값을 빼줍니다.
+             *
+             * 왜냐하면 좌표 평면과 배열의 행(y) 인덱스는 반대로 되어 있기 때문입니다.
+             *
+             * 좌표계에서는 y 값이 클수록 위쪽.
+             * 배열에서는 인덱스가 작을수록 위쪽.
+             * 예를 들어 maxPoint.y = 5이고 p.y = 3이라면:
+             *
+             * ini
+             * 복사
+             * 편집
+             * y = 5 - 3 = 2
+             * 이렇게 하면 y 좌표를 배열의 인덱스로 올바르게 변환할 수 있습니다.
+             */
+            for (Point p : list) {
                 int x = (int) (p.x - minPoint.x); // 이부분 잘 이해 안감!
+                // (p.y -maxPoint.y)의 상황의 역 (*-1)
                 int y = (int) (maxPoint.y - p.y);
 
                 chars[y][x] = '*';
             }
 
             String[] result = new String[chars.length];
-            for(int i = 0; i<result.length; i++){
+            for (int i = 0; i < result.length; i++) {
                 result[i] = new String(chars[i]); //암기
             }
 
             return result;
         }
 
-        public Point getMaxPoint(List<Point> list){
+        public Point getMaxPoint(List<Point> list) {
             long maxX = Long.MIN_VALUE;
             long maxY = Long.MIN_VALUE;
-            for(Point p : list){
+            for (Point p : list) {
                 maxX = Math.max(maxX, p.x);
                 maxY = Math.max(maxY, p.y);
             }
             return new Point(maxX, maxY);
         }
 
-        public Point getMinPoint(List<Point> list){
-            long minX = Long.MAX_VALUE;;
+        public Point getMinPoint(List<Point> list) {
+            long minX = Long.MAX_VALUE;
+            ;
             long minY = Long.MAX_VALUE;
-            for(Point p : list){
+            for (Point p : list) {
                 minX = Math.min(minX, p.x);
                 minY = Math.min(minY, p.y);
             }
             return new Point(minX, minY);
         }
 
-        public List<Point> getPoints(int[][] line){
+        public List<Point> getPoints(int[][] line) {
             List<Point> list = new ArrayList<>();
-            for(int i = 0; i<line.length; i++){
-                for(int j = 0; j<line.length; j++){
-                    if(i == j) continue;
+            for (int i = 0; i < line.length; i++) {
+                for (int j = 0; j < line.length; j++) {
+                    if (i == j) continue;
                     int a = line[i][0];
                     int b = line[i][1];
                     int e = line[i][2];
@@ -98,14 +110,14 @@ public class _1_교점에_별_만들기_책풀이 {
                     int d = line[j][1];
                     int f = line[j][2];
 
-                    long denominator = (long) a*d - (long) b*c;
-                    if(denominator == 0){
+                    long denominator = (long) a * d - (long) b * c;
+                    if (denominator == 0) {
                         continue;
                     }
-                    long numerator1 = (long) b*f - (long) e*d;
-                    long numerator2 = (long) e*c - (long) a*f;
+                    long numerator1 = (long) b * f - (long) e * d;
+                    long numerator2 = (long) e * c - (long) a * f;
 
-                    if(numerator1 % denominator != 0 || numerator2 % denominator != 0){
+                    if (numerator1 % denominator != 0 || numerator2 % denominator != 0) {
                         continue;
                     }
                     long x = numerator1 / denominator;
@@ -114,6 +126,16 @@ public class _1_교점에_별_만들기_책풀이 {
                 }
             }
             return list;
+        }
+
+        class Point {
+            long x;
+            long y;
+
+            Point(long x, long y) {
+                this.x = x;
+                this.y = y;
+            }
         }
     }
 }
---
+++ CodingTest/src/_3_프로그래머스_코딩_테스트_문제_풀이_전략_자바편/_1_배열/_2_삼각_달팽이_내풀이.java
@@ -0,0 +1,84 @@
+package _3_프로그래머스_코딩_테스트_문제_풀이_전략_자바편._1_배열;
+
+public class _2_삼각_달팽이_내풀이 {
+
+    // down, right, up이 일정함 (0이 아니라면 채워져 있는 것.)
+    class Solution {
+        public int[] solution(int n) {
+            String strFlag = "DOWN";
+            int[][] arr = new int[n][n];
+            int limitCnt = getLimitCnt(n);
+            recur(arr, limitCnt, 0, "DOWN", 0, 0);
+
+            for(int i = 0; i < arr.length; i++){
+                for(int j = 0; j < arr[i].length; j++){
+                    System.out.print(arr[i][j] + " ");
+                }
+                System.out.println();
+            }
+            return null;
+        }
+
+        public void recur(int[][] arr, int limitCnt, int nowCnt, String strFlag, int row, int col){
+            if(nowCnt == limitCnt){
+                return;
+            }
+            arr[col][row] = nowCnt + 1;
+
+            String nextStrFlag = null;
+            int nextRow = -99;
+            int nextCol = -99;
+            if(strFlag.equals("DOWN")){
+                nextStrFlag = "DOWN";
+                nextRow = row + 1;
+                nextCol = col;
+                if(nextRow >= arr.length || arr[nextRow][nextCol] != 0){
+                    nextStrFlag = "RIGHT";
+                    nextRow = row;
+                    nextCol = col + 1;
+                }
+
+            }else if(strFlag.equals("RIGHT")){
+                nextStrFlag = "RIGHT";
+                nextRow = row;
+                nextCol = col + 1;
+                if(nextCol >= arr[nextRow].length || arr[nextRow][nextCol] != 0){
+                    nextStrFlag = "UP";
+                    nextRow = row - 1;
+                    nextCol = col - 1;
+                }
+
+            }else if(strFlag.equals("UP")){
+                nextStrFlag = "UP";
+                nextRow = row - 1;
+                nextCol = col - 1;
+                if(nextRow < 0){
+                    System.out.println("nextRow == " + nextRow);
+                    return;
+                }
+                if(nextCol < 0){
+                    System.out.println("nextCol == " + nextCol);
+                    return;
+                }
+                if(arr[nextRow][nextCol] != 0){
+                    nextStrFlag = "DOWN";
+                    nextRow = row + 1;
+                    nextCol = col;
+                }
+            }else{
+                System.out.println("잘못된flag : " + strFlag);
+                return;
+            }
+
+            recur(arr, limitCnt, nowCnt + 1, nextStrFlag, nextRow, nextCol);
+        }
+
+        public int getLimitCnt(int n){
+            int sum = 0;
+            for(int i = 1; i <= n; i++){
+                sum += i;
+            }
+            return sum;
+        }
+    }
+}
