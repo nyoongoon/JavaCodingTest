d3e179cb2ad9e84702bc3c5171f9e3e8928dc09e
nyoongoon <nyoongoon@naver.com>
2025-03-05 07:15:47 +0900

    _6_해시테이블

---
+++ CodingTest/src/_2_Java_Algo_Interview/_6_해시테이블/_2_보석과_돌_내풀이.java
@@ -0,0 +1,24 @@
+package _2_Java_Algo_Interview._6_해시테이블;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class _2_보석과_돌_내풀이 {
+    class Solution {
+        public int numJewelsInStones(String jewels, String stones) {
+            Set<Character> set = new HashSet<>();
+            for(int i = 0 ; i<jewels.length(); i++){
+                char c = jewels.charAt(i);
+                set.add(c);
+            }
+            int cnt = 0;
+            for(int i = 0; i<stones.length(); i++){
+                char c = stones.charAt(i);
+                if(set.contains(c)){
+                    cnt++;
+                }
+            }
+            return cnt;
+        }
+    }
+}
---
+++ CodingTest/src/_3_프로그래머스_코딩_테스트_문제_풀이_전략_자바편/_2_문자열/_2_시저_암호_책풀이.java
@@ -0,0 +1,13 @@
+package _3_프로그래머스_코딩_테스트_문제_풀이_전략_자바편._2_문자열;
+
+/**
+ * 대소문자 동일하게 offset 개념으로 깔끔하게 푸는 것 암기
+ * Character.isAlphabetic() 암기
+ */
+public class _2_시저_암호_책풀이 {
+    class Solution {
+        public String solution(String s, int n) {
+            다시 안보고 풀기..
+        }
+    }
+}
