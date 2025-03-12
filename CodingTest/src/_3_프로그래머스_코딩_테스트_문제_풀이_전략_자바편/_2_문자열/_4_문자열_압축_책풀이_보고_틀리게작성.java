1be57f4fdd332fbd1059bd26c0fef36295f4ec33
nyoongoon <nyoongoon@naver.com>
2025-03-12 19:16:22 +0900

    문자열_압축_책풀이

--- CodingTest/src/_3_프로그래머스_코딩_테스트_문제_풀이_전략_자바편/_2_문자열/_4_문자열_압축_책풀이_보고_틀리게작성.java
+++ CodingTest/src/_3_프로그래머스_코딩_테스트_문제_풀이_전략_자바편/_2_문자열/_4_문자열_압축_책풀이_보고_틀리게작성.java
@@ -8,15 +8,16 @@ import java.util.List;
  * 앞에서부터 고정길이로 자르는 요구사항 주의
  * <p>
  * int를 StringBuilder 추가하고 싶을 때 -> 그냥 append(int i) 하면 됨
- *
- *  if(prevCnt != 0){ // todo prevCnt > 1 --> 왜 틀렸는지 분석하기..
- *
+ * <p>
+ * if(prevCnt != 0){ // todo prevCnt > 1 --> 왜 틀렸는지 분석하기..
+ * --> 앞의 숫자가 1씩 작게 출력됨 (0부터 카운트해서)
+ * --> aaaaaaaaaabb -> 10a2b 같이 자리수가 변하는 경우 틀림!
  */
-public class _4_문자열_압축_책풀이_보고_틀리게작성
+public class _4_문자열_압축_책풀이_보고_틀리게작성 {
     class Solution {
         public int solution(String s) {
             int min = s.length();
-            for(int i = 1; i <= s.length()/2; i++){
+            for (int i = 1; i <= s.length() / 2; i++) {
                 List<String> list = split(s, i);
                 int len = getLen(list);
                 // System.out.println("len == " + len);
@@ -25,17 +26,17 @@ public class _4_문자열_압축_책풀이_보고_틀리게작성
             return min;
         }
 
-        public int getLen(List<String> list){
+        public int getLen(List<String> list) {
             StringBuilder sb = new StringBuilder();
             int prevCnt = 0;
             String prev = "";
-            for(String s : list){
-                if(prev.equals(s)){
+            for (String s : list) {
+                if (prev.equals(s)) {
                     prevCnt++;
-                }else{
+                } else {
                     // char c = (char)(prevCnt + '0');
                     // System.out.println("prevCnt == " + c);
-                    if(prevCnt != 0){ // todo prevCnt > 1
+                    if (prevCnt != 0) { // todo prevCnt > 1
                         sb.append(prevCnt);
                     }
                     sb.append(prev);
@@ -44,7 +45,7 @@ public class _4_문자열_압축_책풀이_보고_틀리게작성
                     prevCnt = 0; // todo prevCnt = 1;
                 }
             }
-            if(prevCnt != 0){ // todo prevCnt > 1
+            if (prevCnt != 0) { // todo prevCnt > 1
                 sb.append(prevCnt);
             }
             sb.append(prev);
@@ -53,10 +54,9 @@ public class _4_문자열_압축_책풀이_보고_틀리게작성
         }
 
 
-
-        public List<String> split(String s, int len){
+        public List<String> split(String s, int len) {
             List<String> list = new ArrayList<>();
-            for(int i = 0; i<s.length(); i += len){
+            for (int i = 0; i < s.length(); i += len) {
                 int endIdx = i + len > s.length() ? s.length() : i + len;
                 list.add(s.substring(i, endIdx));
             }
